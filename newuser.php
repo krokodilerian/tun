0843c2f9f8a78dcaa21ccaf7ffcdd92806f831f4
Vasil Kolev <vasil@ludost.net>
2015-06-22 22:11:02 +0300

    clean old stuff, show netid when assigning

--- newuser.php
+++ newuser.php
@@ -35,51 +35,13 @@ try {
 
 	if ($i==$maxpfx) throw new Exception("out of networks");
 	
-	echo "Assigning $prefix:$hh::/$range\n";
 
 	pq_e("INSERT INTO assigned_nets (userid, range) VALUES ($id, '$prefix:$hh::/$range')");
+	$res = pq_e("select currval('assigned_nets_id_seq'::regclass) as lastid");
+	$row = pg_fetch_object($res);
+	echo "Assigning id $row->lastid prefix $prefix:$hh::/$range\n";
 
 } catch (Exception $e)  {
 	pg_query("ROLLBACK");
 }
 pg_query("COMMIT");
-exit();
-$res = pg_query("select u.id as userid, u.slug, n.range, l.slug as linkslug, l.linkinfo, l.num as linknum, lt.name, p.name as peername, p.localip4 as peerip, p.id as peerid from users u, assigned_nets n, links l, linktypes lt, peers p where u.id=$uid and n.userid=u.id and l.userid=u.id and lt.id=l.linktype and p.id=l.peer");
-
-$bgp=false;
-if (pg_num_rows($res)>1) $bgp=true;
-
-while ($row = pg_fetch_object($res)) {
-	#var_dump($row);
-
-	$usr_ifname=$row->peername."-ipv6-".$row->linkslug;
-
-	$srv_ifname="i6-".$row->slug."-".$row->linkslug;
-
-	echo "\n\n";
-
-	echo "auto $usr_ifname\n";
-	echo "iface $usr_ifname inet6 v4tunnel\n";
-	echo "\tmode ipip\n";
-	echo "\tttl 225\n";
-	echo "\taddress $linknetpfx$row->peerid:$row->userid:$row->linknum:2\n";
-	echo "\tnetmask 120\n";
-	echo "\tlocal $row->linkinfo\n";
-	echo "\tendpoint $row->peerip\n";
-	if (!$bgp)
-		echo "\tgateway $linknetpfx$row->peerid:$row->userid:$row->linknum:1\n";
-
-	echo "\n\n";
-
-	echo "auto $srv_ifname\n";
-	echo "iface $srv_ifname inet6 v4tunnel\n";
-	echo "\tmode ipip\n";
-	echo "\tttl 225\n";
-	echo "\taddress $linknetpfx$row->peerid:$row->userid:$row->linknum:1\n";
-	echo "\tnetmask 120\n";
-	echo "\tlocal $row->peerip\n";
-	echo "\tendpoint $row->linkinfo\n";
-	if (!$bgp)
-		echo "\tup ip r add $row->range via $linknetpfx$row->peerid:$row->userid:$row->linknum:2\n";
-
-}

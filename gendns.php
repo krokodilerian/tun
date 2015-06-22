56f312fb56f89fcf2b228d5970a7f85ae37a66f4
Vasil Kolev <vasil@ludost.net>
2015-06-22 21:51:10 +0300

    fix naming, add full header

--- gendns.php
+++ gendns.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("config.php");
+require_once(dirname(__FILE__)."/config.php");
 
 $res = pg_query("
 select
@@ -18,7 +18,24 @@ select
 from users u, assigned_nets n, links l, linktypes lt, peers p 
 where n.userid=u.id and l.userid=u.id and lt.id=l.linktype and p.id=l.peer");
 
-$rev="";
+$now = date_create('now');
+
+$rev='$TTL 7200
+; This file was generated automatically by '.$argv[0].' on '.date_format($now,"Y-m-d H:i:s").'
+; Please do not edit manually, as your changes will be overwritten
+@ 7200 SOA  freedns.ludost.net. root.ludost.net. (
+;----------------------- edit here ------------------------------
+                         '.date_format($now,"U").'  ; serial in unix timestamp
+;------------------------^^^^^^^^^^------------------------------
+                         21600        ; refresh (  24 hours)
+                         7200         ; retry   (   2 hours)
+                         360000       ; expire  (1000 hours)
+                         172800 )     ; minimum (   2 days)
+
+@                       IN      NS      tyler.ludost.net.
+                        IN      NS      freedns.ludost.net.
+
+';
 $fwd="";
 
 while ($row = pg_fetch_object($res)) {
@@ -35,14 +52,15 @@ while ($row = pg_fetch_object($res)) {
 	$usern=iptorevdns($userip);
 	$srvn=iptorevdns($srvip);
 
-	$userh="$row->peername.$row->linkslug.$row->slug.ptp.t6.initlab.org";
-	$srvh="$row->localid.$row->linkslug.$row->slug.ptp.t6.initlab.org";
 
-	$rev .= "$srvn.\t\tIN\tPTR\t$srvh.\n";
-	$rev .= "$usern.\t\tIN\tPTR\t$userh.\n";
+	$srvh="$row->peername.$row->linkslug.$row->slug.ptp.t6.initlab.org";
+	$userh="$row->localid.$row->linkslug.$row->slug.ptp.t6.initlab.org";
 
-	$fwd .= "$srvh.\t\tIN\tAAAA\t$srvip\n";
-	$fwd .= "$userh.\t\tIN\tAAAA\t$userip\n";
+	$rev .= "$srvn.\tIN\tPTR\t$srvh.\n";
+	$rev .= "$usern.\tIN\tPTR\t$userh.\n";
+
+	$fwd .= "$srvh.\tIN\tAAAA\t$srvip\n";
+	$fwd .= "$userh.\tIN\tAAAA\t$userip\n";
 
 }
 
@@ -52,7 +70,7 @@ union
 select r.addr, r.name from revdns r where userid=0;");
 
 while ($row = pg_fetch_object($res)) {
-	$rev .= iptorevdns($row->addr).".\t\tIN\tPTR\t".$row->name.".\n";
+	$rev .= iptorevdns($row->addr).".\tIN\tPTR\t".$row->name.".\n";
 }
 
 $res = pg_query("
@@ -60,10 +78,8 @@ select ip6r_split(n.range, 64) as addr, r.server from assigned_nets n, revdns_de
 ");
 
 while ($row = pg_fetch_object($res)) {
-	$rev .= iptorevdns64($row->addr).".\t\tIN\tNS\t".$row->server.".\n";
+	$rev .= iptorevdns64($row->addr).".\t\t\t\t\tIN\tNS\t".$row->server.".\n";
 }
-
-echo $rev; exit();
 $f = fopen($revfilename, "w+");
 fwrite($f, $rev);
 fclose($f);
